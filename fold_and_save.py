48319ac1ee5a5c27d271a4518fc2daaef89b5302
nataliemaus <natalietmaus@gmail.com>
2023-05-01 19:48:08 -0400

    wip

--- fold_and_save.py
+++ fold_and_save.py
@@ -8,7 +8,14 @@ target_id = "sample587"
 tm_score = 0.7616
 best_seq = "AQRADQAELAAAAQASAAAAAAAAAAAMMMQLQVVVANNINMTTTDDDDQILQVANDAFDDDFGFGAGHLQQSNIIQNIIQALNNNSNDDDMSVLLLLGDALANWGV"
 
+
 # save_path = "./output.pdb"
 save_path = f"optimal_pdbs_found/{target_id}_{tm_score}.pdb"
 seq_to_pdb(best_seq, save_path=save_path, model=None)
+
+
+tm_score = 0.59744
+best_if_seq = "GAMAALAAERRAAALAAAAAAALAAAEAARAAAAAALGDAAARRAALAAAAAARLAAELGDAALRAALAAAAAARLAALDAAPAAAAAAIAAAARARLEALLA"
+save_path = f"optimal_pdbs_found/{target_id}_{tm_score}_esmif.pdb"
+seq_to_pdb(best_if_seq, save_path=save_path, model=None)
 
